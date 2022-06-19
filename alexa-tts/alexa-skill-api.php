0eb480e151f4be92eb7d315728964ec9324e67b8
Lucas Kinne <developer.pad@outlook.com>
2022-06-20 00:38:09 +0200

    Introduce development mode

--- alexa-tts/alexa-skill-api.php
+++ alexa-tts/alexa-skill-api.php
@@ -32,8 +32,11 @@ class AlexaSkillApi {
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+        if (getenv('DEVELOPMENT_MODE') !== false) {
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        }
 
         $response = curl_exec($ch);
         
