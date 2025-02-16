91083a181e88443bf4ba8956120fa86fd57d09ac
Lucas Kinne <lucas@luckev.info>
2025-02-16 22:25:49 +0100

    Update home appliance to support washing machine notifications

--- alexa-tts/scenarios/home-appliance.php
+++ alexa-tts/scenarios/home-appliance.php
@@ -6,15 +6,18 @@
         protected function assembleText(): string
         {
             $appliance = $this->getOutputAppliance($_POST['appliance']);
-            $status = $this->getOutputStatus($_POST['status']);
+            $status = $this->getOutputStatus($_POST['status'] ?? 'done');
+            $time = $this->getOutputTime(intval($_POST['time'] ?? 0));
 
-            return "Haushalt: $appliance ist $status.";
+            return "Haushalt: $appliance ist $time $status.";
         }
 
         private function getOutputAppliance(string $jobAppliance): string {
             switch ($jobAppliance) {
                 case 'dishwasher':
                     return 'der Geschirrspülgang';
+                case 'washing_machine':
+                    return 'die Waschmaschine';
                 case 'hob':
                     return 'der Kochvorgang';
                 default:
@@ -31,6 +34,32 @@
                 default:
                     return 'in einem unbekannten Zustand';
             }
+        }
+
+        private function getOutputTime(int $seconds): string {
+            $mins = intval(floor($seconds / 60));
+            $secs = $seconds % 60;
+
+            $output = [];
+
+            if ($mins > 0) {
+                $output[] = sprintf(
+                    '%d Minute%s',
+                    $mins,
+                    $mins === 1 ? '' : 'n'
+                );
+            }
+
+            if ($secs > 0) {
+                $output[] = sprintf(
+                    '%d Sekunde%s',
+                    $secs,
+                    $secs === 1 ? '' : 'n'
+                );
+            }
+
+            return count($output) === 0 ? '' :
+                'in ' . implode(' und ', $output);
         }
 
     }
